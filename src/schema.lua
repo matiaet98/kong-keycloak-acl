dee376cffe3f5a8ecad3ab80fd6c5ba82a25e095
matiaet98 <matias.exequiel.estevez@gmail.com>
2020-05-30 02:11:39 -0300

    fix schema file

--- src/schema.lua
+++ src/schema.lua
@@ -4,17 +4,18 @@ return {
     name = "kong-keycloak-acl",
     fields = {
         { consumer = typedefs.no_consumer },
-        config = {
+        { config = {
             type = "record",
             fields = {
                 { token_endpoint = { type = "string", required = true }, },
                 { audience = { type = "string", required = true }, },
                 { resource = { type = "string", required = false }, },
                 { scope = { type = "string", required = false }, },
-            },
-        },
+            }
+        }
+        }
     },
     entity_checks = {
-        { at_least_one_of = {"config.scope", "config.resource"} },
+        { at_least_one_of = {"config.scope", "config.resource"}, },
     },
 }
